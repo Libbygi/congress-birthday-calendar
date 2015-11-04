89f17c299259833123b08bea35c31c96d9da9930
Andrew McGill <arm5077@gmail.com>
2015-11-04 15:03:02 -0500

    add moment.js and some other stuff

--- scripts.js
+++ scripts.js
@@ -1,8 +1,33 @@
 //	$("#date").html();
 	
-	var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-	today = new Date();
-	
-	$("#date").html(days[today.getDay()]);
 
+	
+	
+	var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+
+	var today = new moment().tz("America/New_York");	
+	$("#date").html(today.format("MMM YYYY"));
+
+
+	$.getJSON("data.json", function(data){
+		
+		console.log("Got data!");		
+		
+		data.forEach(function(legislator){
+			
+			var legislator_birthday = new moment(legislator.DOB);
+			
+			if( legislator_birthday.month() == today.month() && legislator_birthday.date() == today.date() ){
+				console.log(legislator.Name);
+			}
+			
+		});
+		
+		
+		// Print the first dude's name
+		// $("#date").html(data[0].Name);
+		
+	});
 	
